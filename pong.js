2929ea6618e7c1bde3a6954ca802b46083405cb7
Alvin1777 <alvin.soderberg@skola.taby.se>
2023-05-08 09:24:19 +0200

    fixat problem

--- pong.js
+++ pong.js
@@ -58,17 +58,15 @@ function checkBounce() {
 
 function updateGameArea() {
     myGameArea.clear();
-    
     player1.speedY = 0;    
-    
-    if (myGameArea.keys && myGameArea.keys[87]) {player1.speedY = -1; }
-    if (myGameArea.keys && myGameArea.keys[83]) {player1.speedY = 1; }
+    if (player1.y <= 400) {if (myGameArea.keys && myGameArea.keys[83]) {player1.speedY = 5; }}    
+    if (player1.y >= 0 ) {if (myGameArea.keys && myGameArea.keys[87]) {player1.speedY = -5; }}
     player1.newPos();    
     player1.update();
 
     player2.speedY = 0;    
-    if (myGameArea.keys && myGameArea.keys[79]) {player2.speedY = -1; }
-    if (myGameArea.keys && myGameArea.keys[76]) {player2.speedY = 1; }
+    if (player2.y <= 400) {if (myGameArea.keys && myGameArea.keys[76]) {player2.speedY = 5; }}
+    if (player2.y >=0 ) {if (myGameArea.keys && myGameArea.keys[79]) {player2.speedY = -5; }}
     player2.newPos();    
     player2.update();
 
