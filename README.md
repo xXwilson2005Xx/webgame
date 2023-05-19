45a2e825fb5ba952987b7bfb43a988f072f6cb56
Alvin1777 <alvin.soderberg@skola.taby.se>
2023-05-19 21:26:20 +0200

    Kommentarer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # java-projekt
+
+
+
+Figuererna styrs genom att hålla ned W eller S tangernterna för spelare 1
+
+Figuererna styrs genom att hålla ned O eller L tangernterna för spelare 2
--- pong.js
+++ pong.js
@@ -9,44 +9,44 @@ function startGame() {
 }
 
 var myGameArea = {
-  canvas: document.getElementById("superpongaren"), //Hämtar canvas från html
-  start: function () {
-    this.canvas.width = 1200;
-    this.canvas.height = 500;
-    this.context = this.canvas.getContext("2d");
-    // document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 20);
-    window.addEventListener("keydown", function (e) {
-      myGameArea.keys = myGameArea.keys || [];
-      myGameArea.keys[e.keyCode] = e.type == "keydown";
-    });
-    window.addEventListener("keyup", function (e) {
-      myGameArea.keys[e.keyCode] = e.type == "keydown";
-    });
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-};
+    canvas: document.getElementById("superpongaren"), // Hämtar canvas-elementet från HTML med ID "superpongaren"
+    start: function () {
+      this.canvas.width = 1200; // Sätter bredden på canvas till 1200 pixlar
+      this.canvas.height = 500; // Sätter höjden på canvas till 500 pixlar
+      this.context = this.canvas.getContext("2d"); // Hämtar 2D-kontexten för att rendera på canvas
+      this.interval = setInterval(updateGameArea, 20); // Anropar funktionen "updateGameArea" var 20:e millisekund för att uppdatera spelet
+      window.addEventListener("keydown", function (e) {
+        myGameArea.keys = myGameArea.keys || []; // Initierar en array för att lagra nedtryckta tangenter om den inte redan finns
+        myGameArea.keys[e.keyCode] = e.type == "keydown"; // Lagrar tillståndet för tangenten (true om nedtryckt, false om släppt) i keys-arrayen
+      });
+      window.addEventListener("keyup", function (e) {
+        myGameArea.keys[e.keyCode] = e.type == "keydown"; // Uppdaterar tillståndet för tangenten i keys-arrayen när den släpps
+      });
+    },
+    clear: function () {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); // Rensar hela canvas genom att sudda ut pixlarna inom det definierade rektangelområdet
+    },
+  };
 
-function component(width, height, color, x, y, speedX, speedY) {
-  this.gamearea = myGameArea;
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y;
-  this.speedX = speedX;
-  this.speedY = speedY;
-  this.update = function () {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  };
-  this.newPos = function () {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  };
-}
+  function component(width, height, color, x, y, speedX, speedY) {
+    this.gamearea = myGameArea; // Referens till spelområdet (myGameArea)
+    this.width = width; // Bredden på komponenten
+    this.height = height; // Höjden på komponenten
+    this.x = x; // X-koordinat för komponentens position
+    this.y = y; // Y-koordinat för komponentens position
+    this.speedX = speedX; // Hastighet i X-riktning
+    this.speedY = speedY; // Hastighet i Y-riktning
+    this.update = function () {
+      ctx = myGameArea.context; // Kontext för rendering på spelområdet
+      ctx.fillStyle = color; // Fyllfärg för komponenten
+      ctx.fillRect(this.x, this.y, this.width, this.height); // Ritar en fylld rektangel med komponentens egenskaper
+    };
+    this.newPos = function () {
+      this.x += this.speedX; // Uppdaterar X-koordinaten baserat på hastigheten i X-riktning
+      this.y += this.speedY; // Uppdaterar Y-koordinaten baserat på hastigheten i Y-riktning
+    };
+  }
+  
 
 function checkBounce() {
   if (gameBall.y >= myGameArea.canvas.height - gameBall.height) {
@@ -90,8 +90,8 @@ function checkBounce() {
 
 let points1 = 0;
 let points2 = 0;
-let element1 = document.getElementById("score1");
-let element2 = document.getElementById("score2");
+let element1 = document.getElementById("score1");   //Hämtar score1 från HTML
+let element2 = document.getElementById("score2");   //Hämtar score2 från HTML
 
 var ctx = canvas.getContext("2d");
 
@@ -127,37 +127,42 @@ function points() {
   if (gameBall.x <= 0) {
     points2 += 1;
   }
-  element1.innerHTML = points1;
-  element2.innerHTML = points2;
+  element1.innerHTML = points1;  //Lägger till antalet poäng i HTML
+  element2.innerHTML = points2;  //Lägger till antalet poäng i HTML
 }
-
 function updateGameArea() {
-  myGameArea.clear();
-  player1.speedY = 0;
-  if (player1.y <= 400) {
-    if (myGameArea.keys && myGameArea.keys[83]) {
-      player1.speedY = 5;
+    myGameArea.clear(); // Rensar spelområdet för att uppdatera det med nya element
+  
+    player1.speedY = 0; // Återställer spelare 1:s hastighet i Y-riktning till 0
+  
+    if (player1.y <= 400) { // Kontrollerar om spelare 1:s position är mindre än eller lika med 400
+      if (myGameArea.keys && myGameArea.keys[83]) { // Kontrollerar om det finns tangenttryckningar och om tangenten med keyCode 83 (S-tangenten) är nedtryckt
+        player1.speedY = 5; // Sätter spelare 1:s hastighet i Y-riktning till 5 (neråt)
+      }
     }
-  }
-  if (player1.y >= 0) {
-    if (myGameArea.keys && myGameArea.keys[87]) {
-      player1.speedY = -5;
+  
+    if (player1.y >= 0) { // Kontrollerar om spelare 1:s position är större än eller lika med 0
+      if (myGameArea.keys && myGameArea.keys[87]) { // Kontrollerar om det finns tangenttryckningar och om tangenten med keyCode 87 (W-tangenten) är nedtryckt
+        player1.speedY = -5; // Sätter spelare 1:s hastighet i Y-riktning till -5 (uppåt)
+      }
     }
-  }
-  player1.newPos();
-  player1.update();
-
-  player2.speedY = 0;
-  if (player2.y <= 400) {
-    if (myGameArea.keys && myGameArea.keys[76]) {
-      player2.speedY = 5;
+  
+    player1.newPos(); // Uppdaterar spelare 1:s position baserat på dess hastighet
+    player1.update(); // Uppdaterar ritningen av spelare 1 på spelområdet
+  
+    player2.speedY = 0; // Återställer spelare 2:s hastighet i Y-riktning till 0
+  
+    if (player2.y <= 400) { // Kontrollerar om spelare 2:s position är mindre än eller lika med 400
+      if (myGameArea.keys && myGameArea.keys[76]) { // Kontrollerar om det finns tangenttryckningar och om tangenten med keyCode 76 (L-tangenten) är nedtryckt
+        player2.speedY = 5; // Sätter spelare 2:s hastighet i Y-riktning till 5 (neråt)
+      }
     }
-  }
-  if (player2.y >= 0) {
-    if (myGameArea.keys && myGameArea.keys[79]) {
-      player2.speedY = -5;
+  
+    if (player2.y >= 0) { // Kontrollerar om spelare 2:s position är större än eller lika med 0
+      if (myGameArea.keys && myGameArea.keys[79]) { // Kontrollerar om det finns tangenttryckningar och om tangenten med keyCode 79 (O-tangenten) är nedtryckt
+        player2.speedY = -5; // Sätter spelare 2:s hastighet i Y-riktning till -5 (uppåt)
+      }
     }
-  }
   player2.newPos();
   player2.update();
 
